c0ac43a2e6b66e06058a6a1563186f4b62e64dcb
amr mustafa <amrmustafa6051@gmail.com>
2022-12-04 02:58:23 +0200

    modfiy database class

--- src/database/DataBase.java
+++ src/database/DataBase.java
@@ -7,69 +7,69 @@ import java.util.Vector;
 // use singleton  
 public class DataBase {
 	private static DataBase instance = null;
-    private Vector<User> users = new Vector<>();
-    private HashMap<String, Service> services = new HashMap<String, Service>();
-    private static HashMap<String, Integer> discounts = new HashMap<String, Integer>();
-    private HashMap<Transaction, String> refundTransactions = new HashMap<Transaction, String>();
+	private Vector<User> users = new Vector<>();
+	private HashMap<String, Service> services = new HashMap<String, Service>();
+	private static HashMap<String, Integer> discounts = new HashMap<String, Integer>();
+	private HashMap<Integer, String> refundTransactions = new HashMap<Integer, String>();
 
-    private int overAllDiscounts;
-    private User currentUser = null;
+	private int overAllDiscounts;
+	private User currentUser = null;
 
-    public static DataBase getInstance() {
+	public static DataBase getInstance() {
 
-        if (instance == null) {
-            instance = new DataBase();
-            discounts.put("Mobile recharge", 10);
-            discounts.put("internet recharge", 0);
-            discounts.put("Landline", 0);
-            discounts.put("Donations ", 0);
+		if (instance == null) {
+			instance = new DataBase();
+			discounts.put("Mobile recharge", 10);
+			discounts.put("internet recharge", 0);
+			discounts.put("Landline", 0);
+			discounts.put("Donations ", 0);
 
-        }
+		}
 
-        return instance;
-    }
+		return instance;
+	}
 
-    public Vector<User> getUsers() {
-        return users;
-    }
+	public Vector<User> getUsers() {
+		return users;
+	}
 
-    public void addUser(String userName, String email, String password) {
-        users.add(new User(userName, email, password));
-    }
+	public void addUser(String userName, String email, String password) {
+		users.add(new User(userName, email, password));
+	}
 
-    public HashMap<String, Service> getServices() {
-        return services;
-    }
+	public HashMap<String, Service> getServices() {
+		return services;
+	}
 
-    public HashMap<String, Integer>  getDiscount() {
-        return discounts;
-    }
+	public HashMap<String, Integer> getDiscount() {
+		return discounts;
+	}
 
-    public void addDiscounts(String service, Integer discount) {
-        discounts.replace(service, discount);
-    }
+	public void addDiscounts(String service, Integer discount) {
+		discounts.replace(service, discount);
+	}
 
-    public int getOverAllDiscounts() {
-        return overAllDiscounts;
-    }
+	public int getOverAllDiscounts() {
+		return overAllDiscounts;
+	}
 
-    public void setOverAllDiscounts(int overAllDiscounts) {
-        this.overAllDiscounts = overAllDiscounts;
-    }
+	public void setOverAllDiscounts(int overAllDiscounts) {
+		this.overAllDiscounts = overAllDiscounts;
+	}
 
-    public void setCurrentUser(User user) {
-        this.currentUser = user;
-    }
+	public void setCurrentUser(User user) {
+		this.currentUser = user;
+	}
 
-    public User getCurrentUser() {
-        return this.currentUser;
-    }
+	public User getCurrentUser() {
+		return this.currentUser;
+	}
 
-    public void addRefundRequest(Transaction t) {
-        refundTransactions.put(t, "waiting");
-    }
+	public void addRefundRequest(Integer id) {
+		refundTransactions.put(id, "waiting");
+	}
 
-    public HashMap<Transaction, String>  getTransaction( ) {
-        return this.refundTransactions;
-    }
+	public HashMap<Integer, String> getTransaction() {
+		return this.refundTransactions;
+	}
 }
