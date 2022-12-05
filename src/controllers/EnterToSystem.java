765333dc6d3d02df6eabd2899aa299817536932f
amrmustafa02 <amrmustafa6051@gmail.com>
2022-12-05 19:55:31 +0200

    last modify 2

--- src/controllers/EnterToSystem.java
+++ src/controllers/EnterToSystem.java
@@ -9,18 +9,28 @@ public class EnterToSystem {
 
 	private DataBase db = DataBase.getInstance();
 
+	public Boolean UserIsExist(Vector<User> users, String email) {
+		for (int i = 0; i < users.size(); i++) {
+			if (email.equals(users.get(i).getEmail())) {
+				return true;
+			}
+		}
+		return false;
+
+	}
+
 	public Boolean signUp(String userName, String email, String password) {
 		// get users from database
 		Vector<User> users = db.getUsers();
 
-		for (int i = 0; i < users.size(); i++) {
-			if (email.equals(users.get(i).getEmail())) {
-				return false;
-			}
+		Boolean check = UserIsExist(users, email);
+
+		if (!check) {
+			db.addUser(userName, email, password);
+			return true;
 		}
 
-		db.addUser(userName, email, password);
-		return true;
+		return false;
 
 	}
 
